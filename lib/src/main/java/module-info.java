7956498100a505232861617d6ee773644fc68318
netcodedev <benmondini@netcode.dev>
2024-02-14 16:12:34 +0100

    Add exports for networking protocols

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -4,6 +4,9 @@
  * It requires the OpenNetLib module and the Lombok library.
  */
 module dev.bitbite.networking.protocols {
+    exports dev.bitbite.networking.protocols;
+    exports dev.bitbite.networking.protocols.http;
+
     requires transitive dev.bitbite.opennetlib;
     requires lombok;
 }
